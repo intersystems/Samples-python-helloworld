381335240e9bfe78408cd7a9d6aa263222eac159
DuongDo-InterSystems <Duong.Do@intersystems.com>
2019-02-28 15:21:51 -0500

    Add sample helloworld code

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+import irisnative
+
+
+def run():
+    ip = "localhost"
+    port = 51777
+    namespace = "USER"
+    username = "SuperUser"
+    password = "SYS"
+
+    connection = irisnative.createConnection(ip, port, namespace, username, password)
+    print("Hello World! You have successfully connected to InterSystems IRIS.")
+
+
+if __name__ == '__main__':
+    run()
